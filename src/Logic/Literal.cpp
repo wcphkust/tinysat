cf59300d80790e801c72260a1655a56fc987b065
wangchengpeng <mhnfwln@126.com>
2020-02-04 15:32:05 +0800

    add Literal and refactor LiteralTerm

---
+++ src/Logic/Literal.cpp
@@ -0,0 +1,5 @@
+//
+// Created by chasen on 4/2/20.
+//
+
+#include "Literal.h"
---
+++ src/Logic/Literal.h
@@ -0,0 +1,14 @@
+//
+// Created by chasen on 4/2/20.
+//
+
+#ifndef TINYSAT_LITERAL_H
+#define TINYSAT_LITERAL_H
+
+
+class Literal {
+
+};
+
+
+#endif //TINYSAT_LITERAL_H
