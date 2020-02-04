212a1a955b804fb6b540a7bf2498485e0a8126ef
wangchengpeng <mhnfwln@126.com>
2020-02-04 14:46:24 +0800

    upload cnfreader.cpp

---
+++ src/IOUtil/cnfreader.cpp
@@ -0,0 +1,105 @@
+//
+// Created by chasen on 2/2/2020.
+//
+
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <stdio.h>
+#include <unistd.h>
+#include "../Logic/CNF.h"
+#include "../Logic/Utils.h"
+
+using std::stringstream;
+
+const string EQUATION_BASE = "/Users/chasen/Documents/CodeBase/Solver/tinysat/data/";
+
+string trim(string s)
+{
+    const char ch = ' ';
+    s.erase(s.find_last_not_of(" ") + 1);
+    s.erase(0, s.find_first_not_of(" "));
+    return s;
+}
+
+
+DisClause getClauseTermFileByLine(string s) {
+    vector<string> stokens;
+    string token;
+    stringstream input(s);
+    while(input >> token) {
+        stokens.push_back(token);
+    }
+
+    vector<LiteralTerm> p_literal_items;
+    for (int i = 0; i < stokens.size() - 1; i++) {
+        Sign sn = (stokens[i] == "+") ? pos : neg;
+        int j = i + 1;
+        LiteralTerm term(stokens[j], sn);
+        i = j;
+        p_literal_items.push_back(term);
+    }
+    DisClause disClauseItem(p_literal_items);
+
+    return disClauseItem;
+}
+
+
+CNF getCNFFromFile(string filename="equation1") {
+    string s;
+    vector<DisClause> p_disjunctive_clause_items;
+    std::ifstream fin(EQUATION_BASE + filename);
+
+    while(getline(fin, s)) {
+        DisClause disClauseItem = getClauseTermFileByLine(s);
+        p_disjunctive_clause_items.push_back(disClauseItem);
+    }
+
+    CNF cnf(p_disjunctive_clause_items);
+    return cnf;
+}
+
+
+LiteralTerm getLiteralTermFromConsoleLine(string s) {
+    int index = s.find("|");
+    string literal = trim(s.substr(0, index));
+    string sign = trim(s.substr(index + 1));
+    Sign sn = (sign == "-") ? neg : pos;
+    LiteralTerm term(literal, sn);
+    return term;
+}
+
+
+// TODO Need to Debug
+CNF getCNFFromConsole() {
+    std::cout << "input the number of disjunctions" << std::endl;
+    int n;
+    string s;
+
+    std::cin >> n;
+    int sign = 0;
+    vector<DisClause> p_disjunctive_clause_items;
+
+    for (int i = 0; i < n; i++) {
+        vector<LiteralTerm> p_literal_items;
+        while(true) {
+            getline(std::cin, s);
+            if (s.empty()) {
+                break;
+            }
+            LiteralTerm t = getLiteralTermFromConsoleLine(s);
+            if (t.getString() == "EMPTY_CLAUSE") {
+                DisClause clause(p_literal_items);
+                p_disjunctive_clause_items.push_back(clause);
+                break;
+            } else {
+                p_literal_items.push_back(t);
+            }
+        }
+    }
+
+    CNF cnf = CNF(p_disjunctive_clause_items);
+    return cnf;
+}
+
